aa636fb9d9c54eac627656e495376a734f0cf8b0
shivnathyadav <95693049+shivnathyadav@users.noreply.github.com>
2022-03-14 10:05:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // components
 import Title from "./components/Title"
-import Description from "./components/Description"
+
 
 // css
 import "./css/style.css"
@@ -8,9 +8,9 @@ import "./css/style.css"
 const App = () => {
 	return (
 		<div className="container">
-			{/* including the Title as well as Description components */}
+			{/* including the Title  */}
 			<Title />
-			<Description />
+			
 		</div>
 	)
 }
